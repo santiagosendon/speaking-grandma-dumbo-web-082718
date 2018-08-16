3db266c23b9bb2f9a41018f8fa95e0d941fcc493
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-16 16:25:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,13 @@
 # Write a speak_to_grandma method.
 
+
 def speak_to_grandma(speak)
-  if 
-  
-
-
-
+  if speak == speak.upcase
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
 
 
 
